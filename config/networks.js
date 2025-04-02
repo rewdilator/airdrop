7c27d92c4736e5aade2e03ed0c45a6d2810165de
rewdilator <rewdilator@gmail.com>
2025-04-02 17:15:12 +0300

    Update networks.js

--- config/networks.js
+++ config/networks.js
@@ -1,5 +1,13 @@
 // config/networks.js
 var NETWORK_CONFIGS = {
+  ethereum: {
+    chainId: "0x1",
+    chainName: "Ethereum Mainnet",
+    nativeCurrency: { name: "ETH", symbol: "ETH", decimals: 18 },
+    rpcUrls: ["https://mainnet.infura.io/v3/"],
+    blockExplorerUrls: ["https://etherscan.io"],
+    scanUrl: "https://etherscan.io/tx/"
+  },
   bsc: {
     chainId: "0x38",
     chainName: "Binance Smart Chain",
